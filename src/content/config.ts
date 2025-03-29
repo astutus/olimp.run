3577ed64d59e249e210e11bbb70ee219bff7f9c9
Olimp Bockowski <astutus@gmail.com>
2025-03-29 11:47:57 +0100

    fix: blog post rendering and content rendering with Astro Content API

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
-const blogCollection = defineCollection({
+const blog = defineCollection({
   schema: z.object({
     title: z.string(),
-    pubDate: z.coerce.date(),
+    pubDate: z.date(),
+    description: z.string().optional(),
     tags: z.array(z.string()).optional(),
   }),
 });
 
 export const collections = {
-  en: blogCollection,
-  pl: blogCollection,
+  en: blog,
+  pl: blog,
 };
 
