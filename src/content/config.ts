7d8fe9cbae21d06ac9aefd340d4d3459bf274646
Olimp Bockowski <astutus@gmail.com>
2025-03-29 14:00:55 +0100

    d-ty fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
-    pubDate: z.date(),
     excerpt: z.string().optional(),
+    pubDate: z.date(),
+    tags: z.array(z.string()).optional()
   }),
 });
 
 export const collections = {
-  en: blog,
-  pl: blog,
+  en: blogCollection,
+  pl: blogCollection
 };
 
