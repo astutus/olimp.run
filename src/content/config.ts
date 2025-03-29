e3159fa513e29ca804468fc934024ef8e83a99f3
Olimp Bockowski <astutus@gmail.com>
2025-03-29 12:16:55 +0100

    not committed changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
-const postCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    tags: z.array(z.string()).optional(),
-  }),
+const blogSchema = z.object({
+  title: z.string(),
+  pubDate: z.date(),
+  description: z.string().optional(),
+  tags: z.array(z.string()).optional()
 });
 
 export const collections = {
-  en: postCollection,
-  pl: postCollection,
+  en: defineCollection({ schema: blogSchema }),
+  pl: defineCollection({ schema: blogSchema }),
 };
 
